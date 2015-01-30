8810a33af80902aaa37503e8894d6f52a075a735
Jenya Cazacu <jenya.cazacu@corvisa.com>
2015-01-30 17:36:54 -0600

    Initial Commit

---
+++ twitterapi/Twitter/config.py
@@ -0,0 +1,7 @@
+#Please put the values here.
+key="QucBkgkTNjXMPusCCQOreEvG9"
+secret="9MtGFwCXJBZGhgbTzzsn9NUzAMqM3kAZWIVgfHmLGWMXKqRih4"
+access_key="1609301214-KbC2QbnwTQwpAW1syXsJyhVszeiiNGxeWJVgywR"
+access_secret="JyhrdPb6Dchubx3nvyAZAG3N9tHSxMizuX23T1ADCLnuO"
+
+
